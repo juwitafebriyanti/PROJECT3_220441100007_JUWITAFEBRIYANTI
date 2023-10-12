e037ecd02beb14f0e3e3c2588cdb42e76b904f11
juwitafebriyanti <113221680+juwitafebriyanti@users.noreply.github.com>
2023-10-12 21:24:42 +0700

    Add files via upload

---
+++ PROJECT3_220441100007_JUWITAFEBRIYANTI/src/main/java/project3/PROJECT3_220441100007_JUWITAFEBRIYANTI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package project3;
+
+/**
+ *
+ * @author juwit
+ */
+public class PROJECT3_220441100007_JUWITAFEBRIYANTI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
